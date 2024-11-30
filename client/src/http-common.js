c1608b92175a5c46734a696258952c8934de1dd6
MahardikaPratama <mahardika.pratama.tif422@pollban.ac.id>
2024-11-30 22:44:53 +0700

    update axios

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://server-production-80b2.up.railway.app/api", // Ganti dengan URL produksi backend
+    baseURL: "dramaku-production.up.railway.app/api", // Ganti dengan URL produksi backend
     headers: {
         "Content-type": "application/json",
     },
